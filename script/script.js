426db927962800e3f49bca939e5bb1247bde05c4
Saulo <saulosilva@super.ufam.edu.br>
2021-08-11 11:50:37 -0400

    Inserida função que mantém a conexão do usuário

--- script/script.js
+++ script/script.js
@@ -2,19 +2,32 @@ const URL_STATUS = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/statu
 const URL_MENSAGE = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
 const URL_PARTICIPANTS = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
 
-function insertParticipant(){
+let userName = { name: ""};
+
+function insertParticipant() {
+    let name = prompt("Escolha um nome de usuário:");
+    userName.name = name;
+    const requestName = axios.post(URL_PARTICIPANTS, userName);
     
-    let name = prompt("Qual o seu nome?");
-    const promise = axios.post(URL_PARTICIPANTS, {name});
+    requestName.then((response)=>{
+        alert("Bem-Vindo " + userName.name + "!!!");
+    });
 
-    promise.then((response)=>{
-        alert(`Bem vindo ao chat Uol: ${name}`);
-    })
-
-    promise.then((err)=>{
-        if(err.request.status != 200){
-             console.log(`Nome de usuário indisponível, tente novamente`);
-        }
-    })
+    requestName.catch((err)=>{
+        const statusCode = err.response.status;
+        if (statusCode != 200) {
+            alert("Esse nome de usuário já foi escolhido! Tente novamente");
+            insertParticipant();
+        } 
+    });
 }
 
+function keepConnection() {
+    const status = axios.post(URL_STATUS, userName);
+    console.log("matendo conexão")
+}
+
+function initChat(){
+    insertParticipant();
+    setInterval(keepConnection, 5000);
+}
