68018fca67e15f9ebf35ead567febd5f25cd1f7d
Saulo <saulosilva@super.ufam.edu.br>
2021-08-11 18:02:52 -0400

    Inserida atualização de participantes

--- script/script.js
+++ script/script.js
@@ -3,8 +3,11 @@ const URL_MENSAGE = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/mess
 const URL_PARTICIPANTS = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
 
 let userName = { name: ""};
+let chosenUser;
+let chosenVisibility;
 
 function insertParticipant() {
+
     let name = prompt("Escolha um nome de usuário:");
     userName.name = name;
     const requestName = axios.post(URL_PARTICIPANTS, userName);
@@ -20,11 +23,12 @@ function insertParticipant() {
             insertParticipant();
         } 
     });
+    menssage();
+    searchPaticipants();
 }
 
 function keepConnection() {
     const status = axios.post(URL_STATUS, userName);
-    console.log("matendo conexão")
 }
 
 function menssage(){
@@ -34,7 +38,7 @@ function menssage(){
 }
 
 function renderMessages(response){
-    console.log("atualizando mensagens");
+
     const mensages = document.querySelector(".mensagens");
     mensages.innerHTML = "";
     const data = response.data;
@@ -76,10 +80,15 @@ function renderMessages(response){
 
 
 function sendMensage(){
+
     let InputMensagem = document.querySelector('#textSend');
-    infoMensage = { from: userName.name, to: "Todos", text: InputMensagem.value, type: "message"};
+    let to = chosenUser.parentNode.querySelector("p").innerHTML;
+    let type = chosenVisibility.parentNode.querySelector("p").innerHTML === "Público" ? "message" : "private_message";
+ 
+    infoMensage = { from: userName.name, to: to, text: InputMensagem.value, type: type};
     console.log(infoMensage);
-    const response = axios.post(URL_MENSAGE, infoMensage);
+
+    /*const response = axios.post(URL_MENSAGE, infoMensage);
 
     response.then((sucess)=>{
         console.log("mensagem enviada com sucesso");
@@ -91,13 +100,77 @@ function sendMensage(){
         console.log("erro ao enviar mensagem");
         window.location.reload();
     });
-
+*/
     InputMensagem.value = "";
 }
 
+function openSideBar(){
+    let side = document.querySelector(".side-bar");
+    side.classList.toggle("hiden");
+}
+
+function hideSideBar(){
+    let side = document.querySelector(".side-bar");
+    side.classList.toggle("hiden");
+}
+
+function messageRecipient(element){
+
+    if(chosenUser) chosenUser.classList.add("hiden");
+    chosenUser = element.querySelector(".check");
+    chosenUser.classList.remove("hiden");
+
+    if(chosenUser.parentNode.querySelector("p").innerHTML === "Todos"){
+        if(chosenVisibility) chosenVisibility.classList.add("hiden");
+
+        chosenVisibility = document.querySelector(".option .check");
+        chosenVisibility.classList.remove("hiden");
+    }
+}
+
+
+function messageVisibility(element){
+
+    if(chosenUser.parentNode.querySelector("p").innerHTML !== "Todos"){
+        if(chosenVisibility) chosenVisibility.classList.add("hiden");
+        
+        chosenVisibility = element.querySelector(".check");
+        chosenVisibility.classList.remove("hiden");
+    }
+}
+
+function searchPaticipants(){
+    const requestParticipants = axios.get(URL_PARTICIPANTS);
+    requestParticipants.then(renderParticipants);
+}
+
+
+function renderParticipants(response){
+
+    const users = document.querySelector(".users");
+    users.innerHTML = "";
+    const data = response.data;
+    const defaultOption = `<div class="user" onclick="messageRecipient(this);">
+                                <ion-icon name="people"></ion-icon>
+                                <p>Todos</p>
+                                <ion-icon name="checkmark-outline" class="check hiden" ></ion-icon>
+                           </div>`;
+    users.innerHTML += defaultOption;
+    for(let i = 0; i < data.length; ++i){
+        let user = `<div class="user" onclick="messageRecipient(this);">
+                        <ion-icon name="person-circle"></ion-icon>
+                        <p>${data[i].name}</p>
+                        <ion-icon name="checkmark-outline" class="check hiden" ></ion-icon>
+                    </div>`;
+
+        users.innerHTML += user;
+    }
+}
 
 function initChat(){
+    
     insertParticipant();
-    setInterval(menssage, 3000);    
+    setInterval(menssage, 3000); 
     setInterval(keepConnection, 5000);
+    setInterval(searchPaticipants, 10000);
 }
