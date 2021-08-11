4950218cfcbb62a8fcc7a93634766f4fe488f4d1
Saulo <saulosilva@super.ufam.edu.br>
2021-08-11 11:51:47 -0400

    Inserida função que renderiza mensagens

--- script/script.js
+++ script/script.js
@@ -27,7 +27,61 @@ function keepConnection() {
     console.log("matendo conexão")
 }
 
+function menssage(){
+    const messages = axios.get(URL_MENSAGE);
+    messages.then(renderMessages);
+
+}
+
+function renderMessages(response){
+    console.log("atualizando mensagens");
+    const mensages = document.querySelector(".mensagens");
+    mensages.innerHTML = "";
+    const data = response.data;
+    for(let i = 0; i < data.length; ++i){
+        let typeMensege = data[i].type;
+        let to = data[i].to;
+        let mensage;
+
+        if(typeMensege === "private_message" && userName === to){
+            mensage = `<div class="mensage ${typeMensege}">${to}
+            <p><span class="time">(${data[i].time})</span>
+            <span class="info">${data[i].from} Reservadamente para ${to}:</span>
+            <span class="text">${data[i].text}</span>
+            </p>
+            </div>`
+        }else if(typeMensege === "status"){
+            mensage = `<div class="mensage ${typeMensege}">
+            <p><span class="time">(${data[i].time})</span>
+            <span class="info">${data[i].from}</span>
+            <span class="text">${data[i].text}</span>
+            </p>
+            </div>`
+        }else{
+            mensage = `<div class="mensage ${typeMensege}">
+            <p><span class="time">(${data[i].time})</span>
+            <span class="info">${data[i].from} <span class="norm">para</span> ${to}:</span>
+            <span class="text">${data[i].text}</span>
+            </p>
+            </div>`
+        }
+
+        mensages.innerHTML += mensage;
+    }
+
+    const lastMessage = document.querySelector('.mensagens div:last-child');
+    lastMessage.scrollIntoView();
+
+}
+
+
+function sendMensage(){
+    console.log("funcionando");
+}
+
+
 function initChat(){
     insertParticipant();
+    setInterval(menssage, 3000);    
     setInterval(keepConnection, 5000);
 }
